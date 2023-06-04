104856ce67a5a9cae137a03bb1a05a247bca751e
Mahefaa <hei.mahefa@gmail.com>
2023-06-04 08:45:48 +0300

    feat: index redirects to profile if authenticated, to login if not

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
+import {GetServerSideProps} from "next";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
     return (
         <>
         </>
     )
+}
+
+export function getServerSideProps({req}: GetServerSideProps) {
+    if (!req.cookies.token) {
+        return {
+            redirect: {
+                destination: "/login",
+                permanent: true
+            }
+        }
+    }
+    return {
+        redirect: {
+            destination: "/profile",
+            permanent: true
+        }
+    }
 }
