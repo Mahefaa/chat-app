55ece2d5177fb9c4f14cf6899bbba655a9904a77
Mahefaa <hei.mahefa@gmail.com>
2023-06-05 22:19:40 +0300

    feat(channel): get by id

--- src/pages/channel/[cid].tsx
+++ src/pages/channel/[cid].tsx
@@ -1,5 +1,27 @@
-export default function ChannelPage() {
+import {Channel, getChannelById} from "@/client";
+
+type channelPageProps = {
+    channel: Channel
+}
+export default function ChannelPage({channel}: channelPageProps) {
+    let {createdAt, id, name, owner, type, updatedAt} = channel;
     return (
-        <></>
+        <>Welcome to {type} channel {name}</>
     );
 };
+
+export async function getServerSideProps({req, query}) {
+    if (!req.cookies.token) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: true
+            }
+        }
+    }
+    const cId = query.cid;
+    const channel = await getChannelById(req.cookies.token, Number(cId))
+        .then((data) => data)
+
+    return {props: {channel}}
+}
