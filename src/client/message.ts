bdb41bd56625bbce1882ac284e75e75a9fbab764
Mahefaa <hei.mahefa@gmail.com>
2023-06-09 20:52:31 +0300

    feat: show messages in get channel by id

---
+++ src/client/message.ts
@@ -0,0 +1,48 @@
+import {Api, getHeaders} from "@/client/global/utils";
+import {User} from "@/client/user";
+import {client} from "@/client/global";
+
+export type Message = {
+    id: number;
+    content: string;
+    recipientId: number;
+    channelId: number;
+    updatedAt: Date;
+    createdAt: Date;
+    senderId: number;
+    sender: Pick<User, "id" | "name" | "email" | "image">;
+};
+
+type MessagePayload = {
+    content: string;
+} & Partial<Pick<Message, "channelId" | "recipientId">>;
+export const getMessagesByChannel = async (
+    token: string,
+    channelId: number
+): Promise<Message[]> => {
+    return await client
+        .get<Api<Message[], "messages">>(`/messages/channel/${channelId}`, getHeaders(token))
+        .then(({data}) => data.messages);
+};
+const sendMessage = async (token: string, payload: MessagePayload): Promise<Message> => {
+    return await client
+        .post<Api<Message, "message">>(`/message`, payload, getHeaders(token))
+        .then(({data}) => data.message);
+};
+export const sendMessageToChannel = async (token: string, channelId: number, content: string) => {
+    return await sendMessage(token, {
+        channelId,
+        content,
+    });
+};
+
+export const sendMessageToRecipient = async (
+    token: string,
+    recipientId: number,
+    content: string
+) => {
+    return await sendMessage(token, {
+        recipientId,
+        content,
+    });
+};
--- src/pages/channel/[cid].tsx
+++ src/pages/channel/[cid].tsx
@@ -1,12 +1,33 @@
 import {Channel, getChannelById} from "@/client";
+import {getMessagesByChannel, Message, sendMessageToChannel} from "@/client/message";
 
 type channelPageProps = {
-    channel: Channel
+    channel: Channel,
+    messages: Message[]
 }
-export default function ChannelPage({channel}: channelPageProps) {
+export default function ChannelPage({channel, messages}: channelPageProps) {
+    const router = useRouter();
     let {createdAt, id, name, owner, type, updatedAt} = channel;
+    const onSubmit = (data) => {
+        sendMessageToChannel(Cookies.get('token')!.toString(), channel.id, data.message)
+            .then(data => {
+                router.reload();
+            })
+    };
     return (
-        <>Welcome to {type} channel {name}</>
+        <>
+            <h1>Welcome to {type} channel {name}</h1>
+            <div>
+                {messages.reverse().map((message) => {
+                    return (
+                        <div key={message.id}>
+                            {`${message.sender.name} - ${message.content}`}
+                        </div>
+                    )
+                })}
+            </div>
+            <MessageForm onSubmit={onSubmit}/>
+        </>
     );
 };
 
@@ -22,6 +43,32 @@ export async function getServerSideProps({req, query}) {
     const cId = query.cid;
     const channel = await getChannelById(req.cookies.token, Number(cId))
         .then((data) => data)
+    const messages = await getMessagesByChannel(req.cookies.token, Number(cId))
+        .then((data) => data)
+    return {props: {channel, messages}}
+}
 
-    return {props: {channel}}
+import {useForm} from 'react-hook-form';
+import Cookies from "js-cookie";
+import {useRouter} from "next/router";
+
+type MessageFormProps = {
+    onSubmit: (data) => void
+}
+
+function MessageForm({onSubmit}: MessageFormProps) {
+    const {register, handleSubmit, reset} = useForm();
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label>Message</label>
+                <textarea
+                    rows={4}
+                    cols={50}
+                    {...register('message', {required: true})}
+                />
+            </div>
+            <button type="submit">Send Message</button>
+        </form>
+    );
 }
