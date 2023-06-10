d92c71ae3d3cf7fa938cfa513255d4b9e2cbbd97
Mahefaa <hei.mahefa@gmail.com>
2023-06-10 07:55:48 +0300

    feat: /channel redirects to channel creation

--- src/pages/channel/index.tsx
+++ src/pages/channel/index.tsx
@@ -1,5 +1,14 @@
+
 export default function MainChannelPage() {
     return (
         <></>
     );
 };
+
+export function getServerSideProps() {
+    return {
+        redirect:{
+            destination: "/channel/create"
+        }
+    }
+}
